3e525cab6810ed08ac919b418a31230bd11fc914
MORIZHIJIANDX <591085210@qq.com>
2022-10-14 00:01:31 +0800

    完善 PipelineStateObject.h

--- DashProject/DashCore/Src/Graphics/PipelineStateObject.h
+++ DashProject/DashCore/Src/Graphics/PipelineStateObject.h
@@ -10,6 +10,7 @@ namespace Dash
 	{
 	public:
 		FPipelineStateObject(const std::wstring& name);
+		virtual ~FPipelineStateObject() {}
 
 		static void DestroyAll();
 
@@ -23,6 +24,8 @@ namespace Dash
 
 		ID3D12PipelineState* GetPipelineState() const { return mPSO; }
 
+		virtual void Finalize() = 0;
+
 	protected:
 		std::wstring mName;
 
@@ -34,13 +37,51 @@ namespace Dash
 	class FGraphicsPSO : public FPipelineStateObject
 	{
 	public:
-		FGraphicsPSO();
+		FGraphicsPSO(const std::wstring& name);
 
 		void SetBlendState(const D3D12_BLEND_DESC& blendDesc);
+		void SetSamplerMask(UINT samperMask);
 		void SetRasterizerState(const D3D12_RASTERIZER_DESC& rasterDesc);
+		void SetDepthStencilState(const D3D12_DEPTH_STENCIL_DESC& depthStencilDesc);
+		void SetPrimitiveTopologyType(D3D12_PRIMITIVE_TOPOLOGY_TYPE primitiveTopologyType);
+		void SetDepthTargetFormat(DXGI_FORMAT depthTargetFormat, UINT massCount = 1, UINT msaaQuality = 0);
+		void SetRenderTargetFormat(DXGI_FORMAT renderTargetFormat, DXGI_FORMAT depthTargetFormat, UINT massCount = 1, UINT msaaQuality = 0);
+		void SetRenderTargetFormats(UINT numRTVs, const DXGI_FORMAT* renderTargetFormats, DXGI_FORMAT depthTargetFormat, UINT massCount = 1, UINT msaaQuality = 0);
+		void SetInputLayout(UINT numElements, const D3D12_INPUT_ELEMENT_DESC* inputElementDescs);
+		void SetPrimitiveRestart(D3D12_INDEX_BUFFER_STRIP_CUT_VALUE indexBufferProps);
+
+		void SetVertexShader(const void* binaryCode, size_t size) { mPSODesc.VS = CD3DX12_SHADER_BYTECODE(binaryCode, size); }
+		void SetPixelShader(const void* binaryCode, size_t size) { mPSODesc.PS = CD3DX12_SHADER_BYTECODE(binaryCode, size); }
+		void SetGeometryShader(const void* binaryCode, size_t size) { mPSODesc.GS = CD3DX12_SHADER_BYTECODE(binaryCode, size); }
+		void SetHullShader(const void* binaryCode, size_t size) { mPSODesc.HS = CD3DX12_SHADER_BYTECODE(binaryCode, size); }
+		void SetDomainShader(const void* binaryCode, size_t size) { mPSODesc.DS = CD3DX12_SHADER_BYTECODE(binaryCode, size); }
+
+		void SetVertexShader(const D3D12_SHADER_BYTECODE& shaderByteCode) { mPSODesc.VS = shaderByteCode; }
+		void SetPixelShader(const D3D12_SHADER_BYTECODE& shaderByteCode) { mPSODesc.PS = shaderByteCode; }
+		void SetGeometryShader(const D3D12_SHADER_BYTECODE& shaderByteCode) { mPSODesc.GS = shaderByteCode; }
+		void SetHullShader(const D3D12_SHADER_BYTECODE& shaderByteCode) { mPSODesc.HS = shaderByteCode; }
+		void SetDomainShader(const D3D12_SHADER_BYTECODE& shaderByteCode) { mPSODesc.DS = shaderByteCode; }
+
+		virtual void Finalize() override;
 
 	private:
 		D3D12_GRAPHICS_PIPELINE_STATE_DESC mPSODesc;
 		std::shared_ptr<const D3D12_INPUT_ELEMENT_DESC> mInputLayout;
 	};
+
+	class FComputePSO : public FPipelineStateObject
+	{
+	public:
+		FComputePSO(const std::wstring& name);
+
+		void SetComputeShader(const void* binaryCode, size_t size) { mPSODesc.CS = CD3DX12_SHADER_BYTECODE(binaryCode, size); }
+		void SetComputeShader(const D3D12_SHADER_BYTECODE& shaderByteCode) { mPSODesc.CS = shaderByteCode; }
+
+		virtual void Finalize() override;
+
+	private:
+		D3D12_COMPUTE_PIPELINE_STATE_DESC mPSODesc;
+	};
+
+
 }
