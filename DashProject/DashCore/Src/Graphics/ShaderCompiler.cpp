a0b06a187311cdee66b451bf97002635feff6bf6
MORIZHIJIANDX <591085210@qq.com>
2022-11-13 14:39:12 +0800

    修改 ShaderCompiler

--- DashProject/DashCore/Src/Graphics/ShaderCompiler.cpp
+++ DashProject/DashCore/Src/Graphics/ShaderCompiler.cpp
@@ -1,18 +1,31 @@
 #include "PCH.h"
-#include <wtypes.h>
-#include <Unknwn.h>
-#include "ThirdParty/dxc/inc/d3d12shader.h"
-#include "ThirdParty/dxc/inc/dxcapi.h"
-#include <wrl.h>
 #include "ShaderCompiler.h"
+#include "Utility/StringUtility.h"
 
 namespace Dash
 {
 	using namespace Microsoft::WRL;
 
-	void ShaderCompiler::CompileShader()
+	void ShaderCompiler::Init()
 	{
-		ComPtr<IDxcCompiler3> compiler;
-		DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&compiler)); 
+		DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&mUtils));
+
+		DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&mCompiler));
+
+		//create default include handler
+		mUtils->CreateDefaultIncludeHandler(&mIncludeHandler);
+	}
+
+	void ShaderCompiler::CompileShader(const std::string& fileName, const std::string& entryPoint)
+	{
+		std::vector<LPCWSTR> args;
+
+		// entry point
+		args.push_back(L"-E");
+		args.push_back(UTF8ToWideString(entryPoint).c_str());
+
+		// prifle
+		args.push_back(L"-E");
+		args.push_back(UTF8ToWideString(entryPoint).c_str());
 	}
 }
--- DashProject/DashCore/Src/Graphics/ShaderCompiler.h
+++ DashProject/DashCore/Src/Graphics/ShaderCompiler.h
@@ -1,11 +1,21 @@
 #pragma once
 
+#include <wtypes.h>
+#include <Unknwn.h>
+#include "ThirdParty/dxc/inc/d3d12shader.h"
+#include "ThirdParty/dxc/inc/dxcapi.h"
+#include <wrl.h>
+
 namespace Dash
 {
 	class ShaderCompiler
 	{
 	public:
+		void Init();
 		void CompileShader();
 	private:
+		Microsoft::WRL::ComPtr<IDxcUtils> mUtils;
+		Microsoft::WRL::ComPtr<IDxcCompiler3> mCompiler;
+		Microsoft::WRL::ComPtr<IDxcIncludeHandler> mIncludeHandler;
 	};
 }
